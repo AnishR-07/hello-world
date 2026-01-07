d195450c3bb60797bd4791ce2e12086efef587cc
manishr363 <manishr363@gmail.com>
2026-01-08 00:07:26 +0530

    comit

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region = "ap-south-1"
+  region = "us-east-1"
 }
 
 module "eks" {
@@ -7,12 +7,12 @@ module "eks" {
   cluster_name    = "hello-world-eks"
   cluster_version = "1.29"
 
-  subnet_ids = ["subnet-xxx", "subnet-yyy"]
-  vpc_id     = "vpc-xxx"
+  subnet_ids = ["subnet-05ef67abd57814ae6", "subnet-00a89e0846cd5c0cb"]
+  vpc_id     = "vpc-027f39b8517af70bc"
 
   eks_managed_node_groups = {
     node_group = {
-      instance_types = ["t3.medium"]
+      instance_types = ["t3.small"]
       desired_size   = 2
     }
   }
