206c3b99bd278c14a9c5695ba36c5949d774050b
manishr363 <manishr363@gmail.com>
2026-01-10 00:59:49 +0530

    comit

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 module "eks" {
   source          = "terraform-aws-modules/eks/aws"
-  version         = "~> 20.0"
+  version         = ""~> 20.0"
   cluster_name    = "hello-world-eks"
   cluster_version = "1.29"
 
